b013ea17705421ed4967628621f96c1d62dc28f5
FirdeusGj <firdegjepali@gmail.com>
2023-07-05 23:27:11 +0200

    added something more

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 export default function Nav() {
   return (
     <div>
-        Navigation bar
+      <nav>
+        <ul>
+          <li>home</li>
+          <li>about</li>
+          <li>contact</li>
+        </ul>
+      </nav>
     </div>
   )
 }
