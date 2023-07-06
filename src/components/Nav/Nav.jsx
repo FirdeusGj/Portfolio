828ec98dcde0a9787a2f57436d5e07dfc3fb3e57
FirdeusGj <firdegjepali@gmail.com>
2023-07-06 23:15:19 +0200

    added more things to the nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React from "react";
+import Logo from "../images/theLogo.png";
 
 export default function Nav() {
   return (
     <div>
       <nav>
-        <ul>
-          <li>home</li>
-          <li>about</li>
-          <li>contact</li>
-        </ul>
+        <div>
+          <img src={Logo} width={100} />
+        </div>
+        <div>
+          <ul>
+            <li>home</li>
+            <li>projects</li>
+            <li>about</li>
+            <li>contact</li>
+            <li>resume</li>
+          </ul>
+        </div>
       </nav>
     </div>
-  )
+  );
 }
