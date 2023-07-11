d3ab22a3f46987becf67048c68d22cc0af40a64c
FirdeusGj <firdegjepali@gmail.com>
2023-07-11 19:34:53 +0200

    finished the technology stack structure

--- src/components/Technology/Technology.jsx
+++ src/components/Technology/Technology.jsx
@@ -3,7 +3,6 @@ import "./Technology.css";
 import CSS from "../images/CSS.png";
 import Firebase from "../images/firebase.png";
 import Git from "../images/git.png";
-import HTML from "../images/html.png";
 import JavaScript from "../images/javascript.png";
 import NextJs from "../images/nextjs.png";
 import ReactLogo from "../images/react.png";
@@ -15,26 +14,52 @@ import Typescript from "../images/typescript.png";
 export default function Technology() {
   return (
     <div>
-      <div>
-        <div>
+      <div className="technology">
+        <div className="proficient-wrapper">
           <h1>Proficient in</h1>
-          <div>
-            <img className="technology" src={HTML} alt="" />
-            <img className="technology" src={CSS} alt="" />
-            <img className="technology" src={JavaScript} alt="" />
-            <img className="technology" src={ReactLogo} alt="" />
-            <img className="technology" src={Firebase} alt="" />
-            <img className="technology" src={Typescript} alt="" />
-            <img className="technology" src={Git} alt="" />
+          <div className="proficient">
+            <figure>
+              <img
+                className="technology-img"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/240px-HTML5_Badge.svg.png"
+                alt=""
+              />
+            </figure>
+            <figure>
+              <img className="technology-img" src={CSS} alt="" />
+            </figure>
+            <figure>
+              <img className="technology-img" src={JavaScript} alt="" />
+            </figure>
+            <figure>
+              <img className="technology-img" src={ReactLogo} alt="" />
+            </figure>
+            <figure>
+              <img className="technology-img" src={Firebase} alt="" />
+            </figure>
+            <figure>
+              <img className="technology-img" src={Typescript} alt="" />
+            </figure>
+            <figure>
+              <img className="technology-img" src={Git} alt="" />
+            </figure>
+            <figure>
+              <img className="technology-img" src={SASS} alt="" />
+            </figure>
           </div>
         </div>
-        <div>
+        <div className="familiar-wrapper">
           <h1>Familiar with</h1>
-          <div>
-            <img className="technology" src={SASS} alt="" />
-            <img className="technology" src={Tailwind} alt="" />
-            <img className="technology" src={NextJs} alt="" />
-            <img className="technology" src={Redux} alt="" />
+          <div className="familiar">
+            <figure>
+              <img className="technology-img" src={Tailwind} alt="" />
+            </figure>
+            <figure>
+              <img className="technology-img" src={NextJs} alt="" />
+            </figure>
+            <figure>
+              <img className="technology-img" src={Redux} alt="" />
+            </figure>
           </div>
         </div>
       </div>
