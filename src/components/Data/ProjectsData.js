1a1db62193288869dabb5253a0a0039efbaa5220
FirdeusGj <firdegjepali@gmail.com>
2023-07-12 23:49:24 +0200

    trying to import the data

---
+++ src/components/Data/ProjectsData.js
@@ -0,0 +1,24 @@
+import CarsProject from '../images/carSuggestionsLink.png'
+
+export const projectsData = [
+    {
+        image: CarsProject,
+        descirption: 'This is made using',
+        languages : 'HTML'
+    },
+    {
+        image: CarsProject,
+        descirption: 'This is made using',
+        languages : 'HTML'
+    },
+    {
+        image: CarsProject,
+        descirption: 'This is made using',
+        languages : 'HTML'
+    },
+    {
+        image: CarsProject,
+        descirption: 'This is made using',
+        languages : 'HTML'
+    },
+]
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { projectsData } from '../Data/ProjectsData';
 
 export default function Projects() {
+  console.log(projectsData.map(elem => elem.languages))
   return (
     <div>
-      
+      <img src={projectsData.map(elem => elem.image)} alt="" />
     </div>
   )
 }
