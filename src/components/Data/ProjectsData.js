ba7bd4376f3033b33f8b3ee670c3a5526d593ed4
FirdeusGj <firdegjepali@gmail.com>
2023-07-13 16:06:21 +0200

    designin the projects

--- src/components/Data/ProjectsData.js
+++ src/components/Data/ProjectsData.js
@@ -1,24 +1,27 @@
-import CarsProject from '../images/carSuggestionsLink.png'
+import CarsProject from "../images/carSuggestionsLink.png";
 
 export const projectsData = [
-    {
-        image: CarsProject,
-        descirption: 'This is made using',
-        languages : 'HTML'
-    },
-    {
-        image: CarsProject,
-        descirption: 'This is made using',
-        languages : 'HTML'
-    },
-    {
-        image: CarsProject,
-        descirption: 'This is made using',
-        languages : 'HTML'
-    },
-    {
-        image: CarsProject,
-        descirption: 'This is made using',
-        languages : 'HTML'
-    },
-]
+  {
+    image: CarsProject,
+    title: "Car Suggestion Project",
+    description: "This is an automotive recommendation platform that presents data on the highest-selling car models of 2022 and provides information on the most affordable prices available to help users find the best deals. The motivation behind this project was to create a unique offering that deviates from the conventional approach of typical car sales websites.",
+    languages: ['HTML', 'CSS'],
+    link: "",
+    githubLink: "",
+  },
+  {
+    image: CarsProject,
+    description: "This is made using",
+    languages: "HTML",
+  },
+  {
+    image: CarsProject,
+    description: "This is made using",
+    languages: "HTML",
+  },
+  {
+    image: CarsProject,
+    description: "This is made using",
+    languages: "HTML",
+  },
+];
