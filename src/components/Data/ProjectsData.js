2d8246fff05f7f302463212f854abe08465b02fb
FirdeusGj <firdegjepali@gmail.com>
2023-07-13 23:05:11 +0200

    finished the projects section

--- src/components/Data/ProjectsData.js
+++ src/components/Data/ProjectsData.js
@@ -5,29 +5,55 @@ import FoodProject from '../images/food app.png'
 import GoogleProject from '../images/googleCloneLink.png'
 import HuluProject from '../images/huluApp.png'
 import WeatherProject from '../images/weatherApp.png'
+import MovieProject from '../images/movieFinder.png'
 
 export const projectsData = [
   {
-    image: CarsProject,
-    title: "Car Suggestion Project",
-    description: "This is an automotive recommendation platform that presents data on the highest-selling car models of 2022 and provides information on the most affordable prices available to help users find the best deals. The motivation behind this project was to create a unique offering that deviates from the conventional approach of typical car sales websites.",
-    languages: ['HTML', 'CSS'],
-    link: "",
-    githubLink: "",
+    image: GoogleProject,
+    title: 'Google Clone',
+    description: "I have developed a personal rendition of a search engine resembling Google. This search engine is designed to retrieve search results based on the user's input, mimicking the functionality of the original Google search engine.",
+    languages: ['HTML', 'CSS', 'JavaScript', 'React', 'APIs'],
+    link: "https://master--charming-capybara-2f2862.netlify.app/",
+    githubLink: "https://github.com/FirdeusGj/Firdeus-Google-Clone",
+  },
+  {
+    image: FitnessProject,
+    title: 'Fitness App',
+    description: "This website represents a fitness application designed to be affiliated with a gym. It offers various fitness plans that can be accessed through monthly subscription services. Furthermore, the website showcases testimonials from satisfied users and utilizes captivating animations to enhance the user experience.",
+    languages: ['HTML', 'CSS', 'JavaScript', 'React'],
+    link: "https://comfy-genie-738686.netlify.app/",
+    githubLink: "https://github.com/FirdeusGj/Fitness-Website",
   },
   {
     image: EcomProject,
-    description: "This is made using",
-    languages: ['HTML', 'CSS', 'Javascript', 'React'],
+    title: 'Ecommerce App',
+    description: "The website in question offers a wide selection of books for purchase, accompanied by advantageous pricing. Additionally, users have the ability to add books to their shopping cart. Furthermore, the website incorporates a filtering feature that enables sorting of books based on price, ranging from the highest to the lowest, as an illustrative example.",
+    languages: ['HTML', 'CSS', 'JavaScript', 'React'],
+    link: "https://imaginative-capybara-188b0a.netlify.app/",
+    githubLink: "https://github.com/FirdeusGj/Library-react",
+  },
+  {
+    image: HuluProject,
+    title: 'Hulu Clone 2.0',
+    description: "I have created an enhanced version of a streaming platform similar to Hulu, referred to as Hulu Clone 2.0, utilizing the Firebase framework. I have developed this streaming service that emulates the core features and functionalities of the original Hulu platform.",
+    languages: ['HTML', 'CSS', 'JavaScript', 'React', 'APIs', 'Firebase'],
+    link: "https://master--playful-kitten-c301f3.netlify.app/",
+    githubLink: "https://github.com/FirdeusGj/Hulu-2.0-React",
+  },
+  {
+    image: MovieProject,
+    title: 'Movie Finder',
+    description: 'The Movie Finder project is a movie search application designed to locate any film. Users can easily locate movies of their choice. The application provides detailed information about each movie. This ensures that users can find their desired movies and also gather information about them.',
+    languages: ['HTML', 'CSS', 'JavaScript', 'React', 'APIs'],
+    link: "https://master--majestic-pika-a8a823.netlify.app/",
+    githubLink: "https://github.com/FirdeusGj/Movie-Finder-ReactApp",
   },
   {
     image: CarsProject,
-    description: "This is made using",
-    languages: "HTML",
-  },
-  {
-    image: CarsProject,
-    description: "This is made using",
-    languages: "HTML",
+    title: "Car Suggestions",
+    description: "This is an automotive recommendation platform that presents data on the highest-selling car models of 2022 and provides information on the most affordable prices available to help users find the best deals. The motivation behind this project was to create a unique offering that deviates from the conventional approach of typical car sales websites.",
+    languages: ['HTML', 'CSS'],
+    link: "https://firdeusgj.github.io/Car-Sales/",
+    githubLink: "https://github.com/FirdeusGj/Car-Sales",
   },
 ];
